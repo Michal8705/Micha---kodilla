cc1ed0743dac1e7a7d58fdbc86bb088f77bb5395
Michal8705 <Michal8705@gmail.com>
2020-04-07 20:44:08 +0200

    Modul 6.2

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public int a;
+    public int b;
+
+    public Calculator(int a, int b) {
+           this.a = a;
+           this.b = b;
+    }
+
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+
+    public int subtractBFromA(int a, int b) {
+        return a - b;
+    }
+}
