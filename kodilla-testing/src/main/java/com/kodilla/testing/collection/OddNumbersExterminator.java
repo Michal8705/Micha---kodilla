f726d0f7f60eb7973bdbf09281565d51f92a3506
Michal8705 <Michal8705@gmail.com>
2020-04-09 13:20:11 +0200

    Modul 6.3

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -2,16 +2,17 @@ package com.kodilla.testing.collection;
 import java.util.*;
 import java.lang.*;
 
-public class OddNumbersExterminator {
-    public void exterminate(ArrayList<Integer> numbers) {
-
-        int number = 0;
-        for (int n = 0; n < numbers.size(); n++) {
-            number = numbers.get(n);
-            if (number % 2 != 0) {
-                numbers.remove(n);
+class OddNumbersExterminator {
+    public ArrayList<Integer> exterminate (ArrayList<Integer> numbers) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (Integer number : numbers) {
+            if (number % 2 == 0) {
+                list.add(number);
             }
         }
+         return list;
     }
 }
+
+
 
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -21,38 +21,26 @@ public class CollectionTestSuite {
     public void testOddNumbersExterminatorEmptyList() {
         //Given
         ArrayList<Integer> list = new ArrayList<Integer>();
-        for (int n = 0; n < 0; n++) {
-            list.add(n);
-        }
         //When
         OddNumbersExterminator task = new OddNumbersExterminator();
-        task.exterminate(list);
         System.out.println("Testing empty list");
         //Then
-        Assert.assertEquals(list.size(), 0);
+        Assert.assertEquals(0, task.exterminate(list).size());
+
     }
 
     @Test
     public void testOddNumbersExterminatorNormalList() {
         //Given
         ArrayList<Integer> list = new ArrayList<Integer>();
-        for (int n = 0; n < 10; n++) {
-            list.add(n);
-        }
+        list.add(1);
+        list.add(4);
+        list.add(20);
         //When
         OddNumbersExterminator task = new OddNumbersExterminator();
-        task.exterminate(list);
-        int number = 0;
-        int result = 0;
-        for (int n = 0; n < list.size(); n++) {
-            number = list.get(n);
-            if (number % 2 != 0) {
-                result = result +1;
-            }
-        }
         System.out.println("Testing normal list");
         //Then
-        Assert.assertEquals(result, 0);
+        Assert.assertEquals(2, task.exterminate(list).size());
 
     }
 }
